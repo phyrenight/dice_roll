deae437e8143bc1eafb48203724c19e33e6ee27c
Marc <phyrenight@gmail.com>
2019-05-05 15:29:05 -0400

    randomNumberGenerator() range is 1 - 5, it should be 1 - 6

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 function randomNumberGenerator(){
     let min = 1;
-    let max = 6;
+    let max = 7;
     return Math.floor(Math.random() * (min - max) + max)
+}
+
+function displayNumber(){
+    console.log(randomNumberGenerator())
 }
